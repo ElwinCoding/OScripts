ae7ef43f733f2ac477e34f9b5f0fce66219ba4dd
Elwin <eshen1998@gmail.com>
2022-05-30 00:17:22 -0600

    Tree chopper

---
+++ src/TestScript.java
@@ -0,0 +1,15 @@
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.ScriptManifest;
+import org.dreambot.api.script.Category;
+
+@ScriptManifest(author = "You", name = "My First Script", version = 1.0, description = "hello", category = Category.WOODCUTTING)
+
+
+public class TestScript extends AbstractScript {
+    @Override
+    public int onLoop() {
+        log("My first script!");
+
+        return 5000;
+    }
+}
