9b38b353695ffaddc261111f59df2b91fcd99279
Elwin <eshen1998@gmail.com>
2022-05-30 21:26:35 -0600

    Tree chopper

---
+++ src/Tutorial.java
@@ -0,0 +1,47 @@
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.ScriptManifest;
+import org.dreambot.api.script.Category;
+import org.dreambot.api.methods.interactive.NPCs;
+import org.dreambot.api.wrappers.interactive.NPC;
+import org.dreambot.api.methods.dialogues.Dialogues;
+import org.dreambot.api.methods.MethodProvider;
+
+import java.awt.*;
+
+@ScriptManifest(author = "You", name = "Tutorial Island", version = 1.0, description = "Getting off Tutorial Island", category = Category.MISC)
+
+
+public class Tutorial extends AbstractScript {
+
+    public void onStart() {
+        log("The script has started.");
+    }
+
+     private boolean Gielinor() {
+         NPC guide = NPCs.closest("Gielinor Guide");
+         if (guide != null) {
+             guide.interact();
+             while (!Dialogues.areOptionsAvailable()) {
+                 Dialogues.clickContinue();
+                 sleep(1000,7000);
+             }
+         }
+         return false;
+     }
+
+    @Override
+    public int onLoop() {
+        if (!Gielinor()) {
+            log("Gielinor has broke.");
+        }
+        else {
+            log("talk sucess.");
+        }
+        return -1;
+    }
+
+    @Override
+    public void onExit() {
+        log("The script has finished.");
+    }
+}
