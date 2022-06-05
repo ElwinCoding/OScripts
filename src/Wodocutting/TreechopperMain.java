7509e56dc8eca80eabca0ed8c5c030c8b32794c3
Elwin <eshen1998@gmail.com>
2022-06-04 19:10:39 -0600

    Good luck

--- src/Wodocutting/TreechopperMain.java
+++ src/Wodocutting/TreechopperMain.java
@@ -5,16 +5,19 @@ import org.dreambot.api.methods.skills.SkillTracker;
 import org.dreambot.api.script.impl.TaskScript;
 import org.dreambot.api.script.ScriptManifest;
 import org.dreambot.api.script.Category;
+import org.dreambot.api.Client;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 @ScriptManifest(author = "You", name = "TreeChopper", version = 1.0, description = "chop trees", category = Category.WOODCUTTING)
 
 
 public class TreechopperMain extends TaskScript {
 
-    private boolean bank;
+    private boolean bank = false;
     private String tree_type;
 
 
@@ -36,15 +39,41 @@ public class TreechopperMain extends TaskScript {
 
 
     public void onExit() {
+        log(bank);
         log("Tree chopper has finished chopping");
     }
 
     private void createGUI() {
 
         JFrame frame = new JFrame();
-        frame.setTitle("Wood Chopper");
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setVisible(true);
+        frame.setTitle("Wood Chopper");
 
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // so only GUI closes when X is pressed
+        frame.setLocationRelativeTo(Client.getInstance().getCanvas()); // centers the GUI to client
+        frame.getContentPane().setLayout(new BorderLayout()); // sets the layout of the GUI
+        frame.setPreferredSize(new Dimension(300, 300)); // default size is according to components
+        frame.pack(); // sizes GUI so all components are visible
+
+        JPanel setting_panel = new JPanel();
+        setting_panel.setLayout(new GridLayout(0, 2));
+
+        JLabel tree_label = new JLabel();
+        tree_label.setText("Tree Type:");
+        setting_panel.add(tree_label);
+
+        // this adds tick boxes
+        JCheckBox bank_check = new JCheckBox();
+        bank_check.setText("Bank Logs");
+
+        bank_check.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bank = true;
+            }
+        });
+        setting_panel.add(bank_check);
+
+        frame.getContentPane().add(setting_panel, BorderLayout.CENTER);
     }
 }
